520b1a9d240bac557e6ab6447eece76fee872af4
Bernhard Saumweber <b.saumweber@gmail.com>
2023-12-05 18:50:40 +0100

    Add generated files

---
+++ data/Raids.lua
@@ -0,0 +1,11 @@
+C.ACTIVITY_TO_MAP_ID = {
+    [1189] = 2522, -- Vault of the Incarnates (Normal)
+    [1190] = 2522, -- Vault of the Incarnates (Heroic)
+    [1191] = 2522, -- Vault of the Incarnates (Mythic)
+    [1235] = 2569, -- Aberrus (Normal)
+    [1236] = 2569, -- Aberrus (Heroic)
+    [1237] = 2569, -- Aberrus (Mythic)
+    [1251] = 2549, -- Amirdrassil, the Dream's Hope (Normal)
+    [1252] = 2549, -- Amirdrassil, the Dream's Hope (Heroic)
+    [1253] = 2549, -- Amirdrassil, the Dream's Hope (Mythic)
+}
