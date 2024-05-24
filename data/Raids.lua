80b717508aa814b7ccd3313adcec978f21b7b207
Bernhard Saumweber <b.saumweber@gmail.com>
2024-05-24 14:08:59 +0200

    Update source tables

--- data/Raids.lua
+++ data/Raids.lua
@@ -5,7 +5,10 @@ C.ACTIVITY_TO_MAP_ID = {
     [1235] = 2569, -- Aberrus (Normal)
     [1236] = 2569, -- Aberrus (Heroic)
     [1237] = 2569, -- Aberrus (Mythic)
-    [1251] = 2549, -- Amirdrassil, the Dream's Hope (Normal)
-    [1252] = 2549, -- Amirdrassil, the Dream's Hope (Heroic)
-    [1253] = 2549, -- Amirdrassil, the Dream's Hope (Mythic)
+    [1251] = 2549, -- Amirdrassil (Normal)
+    [1252] = 2549, -- Amirdrassil (Heroic)
+    [1253] = 2549, -- Amirdrassil (Mythic)
+    [1332] = 1136, -- Siege of Orgrimmar (Normal)
+    [1331] = 1136, -- Siege of Orgrimmar (Heroic)
+    [1330] = 1136, -- Siege of Orgrimmar (Mythic)
 }
