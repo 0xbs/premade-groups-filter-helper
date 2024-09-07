1942132e8552017879172453fddded1ad30d2f02
Bernhard Saumweber <b.saumweber@gmail.com>
2024-09-07 17:01:46 +0200

    Formatting and sorting

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"0xbs/premade-groups-filter-helper/logger"
 	"fmt"
 	"os"
+	"sort"
+	"strings"
 )
 
 //goland:noinspection GoUnhandledErrorResult
@@ -31,13 +33,7 @@ func main() {
 
 	writeRaids(activities)
 
-	act, err := os.Create("data/Activity.lua")
-	if err != nil {
-		panic(err)
-	}
-	defer act.Close()
-
-	fmt.Fprint(act, "C.ACTIVITY = {\n")
+	fmtActivities := []string{}
 	for _, activity := range activities {
 		if difficultyMap[activity.DifficultyID] == 0 {
 			//logger.Infof("unknown difficultyID %d", activity.DifficultyID)
@@ -60,7 +56,7 @@ func main() {
 					mapChallengeModeID, activity.MapChallengeModeID, activity.ID, activity.FullName_lang)
 			}
 		}
-		fmt.Fprintf(act, "    [%4d] = { difficulty = %d, category = %d, mapID = %4d, cmID = %3d }, -- %s\n",
+		fmtActivity := fmt.Sprintf("    [%4d] = { difficulty = %d, category = %d, mapID = %4d, cmID = %3d }, -- %s\n",
 			activity.ID,
 			difficultyMap[activity.DifficultyID],
 			activity.GroupFinderCategoryID,
@@ -68,10 +64,20 @@ func main() {
 			cmID,
 			activity.FullName_lang,
 		)
+		fmtActivities = append(fmtActivities, fmtActivity)
 	}
+	sort.Sort(sort.StringSlice(fmtActivities))
+
+	act, err := os.Create("data/Activity.lua")
+	if err != nil {
+		panic(err)
+	}
+	defer act.Close()
+	fmt.Fprint(act, "C.ACTIVITY = {\n")
+	fmt.Fprint(act, strings.Join(fmtActivities, ""))
 	fmt.Fprint(act, "}\n\n")
-    fmt.Fprint(act, "-- Return a default set if activity not found\n")
-    fmt.Fprint(act, "setmetatable(C.ACTIVITY, { __index = function() return { difficulty = 0, category = 0, mapID = 0, cmID = 0 } end })\n")
+	fmt.Fprint(act, "-- Return a default set if activity not found\n")
+	fmt.Fprint(act, "setmetatable(C.ACTIVITY, { __index = function() return { difficulty = 0, category = 0, mapID = 0, cmID = 0 } end })\n")
 }
 
 func writeWrath() {
@@ -86,42 +92,50 @@ func writeLegacy(source, target string) {
 	defer gfaWrath.Close()
 	activitiesWrath := parseGroupFinderActivityWrath(gfaWrath)
 
-	actWrath, err := os.Create(target)
-	if err != nil {
-		panic(err)
-	}
-	defer actWrath.Close()
-
-	fmt.Fprint(actWrath, "C.ACTIVITY = {\n")
+	fmtActivities := []string{}
 	for _, activity := range activitiesWrath {
 		if difficultyMap[activity.DifficultyID] == 0 {
 			continue
 		}
-		fmt.Fprintf(actWrath, "    [%4d] = { difficulty = %d, category = %3d, mapID = %4d }, -- %s\n",
+		fmtActivity := fmt.Sprintf("    [%4d] = { difficulty = %d, category = %3d, mapID = %4d }, -- %s\n",
 			activity.ID,
 			difficultyMap[activity.DifficultyID],
 			activity.GroupFinderCategoryID,
 			activity.MapID,
 			activity.FullName_lang,
 		)
+		fmtActivities = append(fmtActivities, fmtActivity)
 	}
+	sort.Sort(sort.StringSlice(fmtActivities))
+
+	actWrath, err := os.Create(target)
+	if err != nil {
+		panic(err)
+	}
+	defer actWrath.Close()
+	fmt.Fprint(actWrath, "C.ACTIVITY = {\n")
+	fmt.Fprint(actWrath, strings.Join(fmtActivities, ""))
 	fmt.Fprint(actWrath, "}\n\n")
 	fmt.Fprint(actWrath, "-- Return a default set if activity not found\n")
 	fmt.Fprint(actWrath, "setmetatable(C.ACTIVITY, { __index = function() return { difficulty = 0, category = 0, mapID = 0, cmID = 0 } end })\n")
 }
 
 func writeRaids(activities []GroupFinderActivity) {
+	fmtActivities := []string{}
+	for _, activity := range activities {
+		if activity.ID >= 1189 && (activity.DifficultyID == 14 || activity.DifficultyID == 15 || activity.DifficultyID == 16) {
+			fmtActivity := fmt.Sprintf("    [%4d] = %4d, -- %s\n", activity.ID, activity.MapID, activity.FullName_lang)
+			fmtActivities = append(fmtActivities, fmtActivity)
+		}
+	}
+	sort.Sort(sort.StringSlice(fmtActivities))
+
 	raids, err := os.Create("data/Raids.lua")
 	if err != nil {
 		panic(err)
 	}
 	defer raids.Close()
-
 	fmt.Fprint(raids, "C.ACTIVITY_TO_MAP_ID = {\n")
-	for _, activity := range activities {
-		if activity.ID >= 1189 && (activity.DifficultyID == 14 || activity.DifficultyID == 15 || activity.DifficultyID == 16) {
-			fmt.Fprintf(raids, "    [%4d] = %4d, -- %s\n", activity.ID, activity.MapID, activity.FullName_lang)
-		}
-	}
+	fmt.Fprint(raids, strings.Join(fmtActivities, ""))
 	fmt.Fprint(raids, "}\n")
 }
